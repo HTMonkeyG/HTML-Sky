feb1b52c8108a06f521de18b7606bebff4282258
HTMonkeyG <3254339747@qq.com>
2025-09-09 02:08:37 +0800

    Feat: Create basic user interfaces. Remove ImGui demo window.

---
+++ src/ui/console.h
@@ -0,0 +1,14 @@
+#ifndef __CONSOLE_H__
+#define __CONSOLE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void HTMenuConsole();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- src/ui/gui.cpp
+++ src/ui/gui.cpp
@@ -1,10 +1,14 @@
+#include <windows.h>
 #include "imgui.h"
 #include "imgui_impl_win32.h"
 #include "imgui_impl_vulkan.h"
 #include "vulkan/vulkan.h"
-#include "gui.h"
+#include "ui/input.h"
+#include "ui/gui.h"
+#include "ui/console.h"
 #include "globals.h"
-#include "input.h"
+
+static bool gShowMainMenu = true;
 
 /**
  * Initialize ImGui context and window message hook.
@@ -19,10 +23,52 @@ void HTInitGUI() {
   ImGui::CreateContext();
   ImGui_ImplWin32_Init(gGameStatus.window);
   ImGuiIO &io = ImGui::GetIO();
+  ImGuiStyle &style = ImGui::GetStyle();
   io.IniFilename = io.LogFilename = nullptr;
+  f32 dpiScale = ImGui_ImplWin32_GetDpiScaleForHwnd(gGameStatus.window);
+  style.ScaleAllSizes(dpiScale);
+  io.FontGlobalScale = dpiScale;
   HTInstallInputHook();
 }
 
+/**
+ * Show HTML Menus.
+ */
 void HTUpdateGUI() {
-  ImGui::ShowDemoWindow();
+  // Press "~" key to show or hide.
+  if (GetAsyncKeyState(VK_OEM_3) & 0x1)
+    gShowMainMenu = !gShowMainMenu;
+  
+  if (!gShowMainMenu)
+    return;
+
+  ImGuiStyle &style = ImGui::GetStyle();
+  style.ScrollbarRounding = 0.0f;
+  style.WindowTitleAlign.x = 0.5f;
+  style.WindowTitleAlign.y = 0.5f;
+
+  if (!ImGui::Begin("HTML Main Menu", &gShowMainMenu))
+    return (void)ImGui::End();
+  
+  if (ImGui::BeginTabBar("Nav Main")) {
+    if (ImGui::BeginTabItem("Console")) {
+      HTMenuConsole();
+      ImGui::EndTabItem();
+    }
+    if (ImGui::BeginTabItem("Mods")) {
+      ImGui::EndTabItem();
+    }
+    if (ImGui::BeginTabItem("Settings")) {
+      ImGui::EndTabItem();
+    }
+    if (ImGui::BeginTabItem("Abouts")) {
+      ImGui::Text("HT's Mod Loader v" HTML_VERSION_NAME " by HTMonkeyG");
+      ImGui::Text("A mod loader developed for Sky:CotL.");
+      ImGui::Text("<https://www.github.com/HTMonkeyG/HTML-Sky>");
+      ImGui::EndTabItem();
+    }
+    ImGui::EndTabBar();
+  }
+  
+  ImGui::End();
 }
