87f767de749074e1ab9f3c945833a29356cb9840
HTMonkeyG <3254339747@qq.com>
2025-09-23 00:16:46 +0800

    Change mod folder structure. Add definition of HTKeyEventPreventFlags.

--- src/htinternal.h
+++ src/htinternal.h
@@ -24,6 +24,20 @@ extern "C" {
 // [SECTION] Mod loader functions.
 // ----------------------------------------------------------------------------
 
+static inline i32 fileExists(const wchar_t *path) {
+  DWORD attr = GetFileAttributesW(path);
+  if (attr == INVALID_FILE_ATTRIBUTES || (attr & FILE_ATTRIBUTE_DIRECTORY))
+    return 0;
+  return 1;
+}
+
+static inline i32 folderExists(const wchar_t *path) {
+  DWORD attr = GetFileAttributesW(path);
+  if (attr == INVALID_FILE_ATTRIBUTES || !(attr & FILE_ATTRIBUTE_DIRECTORY))
+    return 0;
+  return 1;
+}
+
 HTStatus HTLoadMods();
 void HTLoadSingleMod();
 void HTUnloadSingleMod();
@@ -37,6 +51,7 @@ HTStatus HTRejectDll();
 
 typedef enum {
   HTHandleType_Invalid = 0,
+  HTHandleType_Manifest,
   HTHandleType_Mod,
   HTHandleType_Hotkey,
   HTHandleType_Command
